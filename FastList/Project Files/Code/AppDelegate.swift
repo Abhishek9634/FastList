da0d86d869adf783dc069bb496a4f9125325cf4b
Abhishek <abhishekthapliyal7579@gmail.com>
2018-10-02 12:09:17 +0530

    chore(files): added files

---
+++ FastList/Project Files/Code/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  FastList
+//
+//  Created by Abhishek Thapliyal on 02/10/18.
+//  Copyright © 2018 Abhishek Thapliyal. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        CoreDataStack.shared.saveContext()
+    }
+}
