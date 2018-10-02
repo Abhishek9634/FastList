0fa8962f9226a31afbf37e791dbc47b1a39569cf
Abhishek <abhishekthapliyal7579@gmail.com>
2018-10-02 12:17:17 +0530

    chore(files): added files

--- FastList/Project Files/Code/UI/LoginViewController.swift
+++ FastList/Project Files/Code/UI/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  FastList
 //
 //  Created by Abhishek Thapliyal on 02/10/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
